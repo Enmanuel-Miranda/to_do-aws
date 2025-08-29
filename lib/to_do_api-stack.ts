61575e07db21fa3b8d55a764d6ccbb09c26dd530
Miranda-Enmanuel <mirandaenmanuel21@gmail.com>
2025-08-29 00:21:36 -0500

    Upload the base project

--- lib/to_do_api-stack.ts
+++ lib/to_do_api-stack.ts
@@ -1,16 +1,49 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class ToDoApiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // base de datos
+    const tableTask = new dynamodb.Table(this,'TableTask', {
+      partitionKey:{ name:'taskId', type: dynamodb.AttributeType.STRING},
+      tableName: 'Task',
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ToDoApiQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // funciones lambda
+    const createTaskFunction = new lambda.Function(this,'CreateTaskFunction',{
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'create-task.handler',
+      code: lambda.Code.fromAsset('lib/lambda'),
+      environment: {TABLE_NAME: tableTask.tableName},
+    });
+    const getTaskFunction =new lambda.Function(this,'GetTaskFunction',{
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'get-task.handler',
+      code: lambda.Code.fromAsset('lib/lambda'),
+      environment: {TABLE_NAME: tableTask.tableName},
+    });
+
+    // permisos
+    tableTask.grantWriteData(createTaskFunction);
+    tableTask.grantReadData(getTaskFunction);
+
+    //La puerta de entrada de la api gateway
+    const api = new apigateway.RestApi(this, 'TaskApi');
+    const taskResource = api.root.addResource('task');
+    const taskIdResource = taskResource.addResource('{id}');
+
+    taskResource.addMethod('POST', new apigateway.LambdaIntegration(createTaskFunction));
+    taskResource.addMethod('GET', new apigateway.LambdaIntegration(getTaskFunction));
+    
+
+
   }
 }
